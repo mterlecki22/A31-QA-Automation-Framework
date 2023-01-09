6e700d9e284c6b9d07ccf1cec40c26e549375014
mterlecki22 <121317526+mterlecki22@users.noreply.github.com>
2023-01-09 14:27:41 -0600

    Update Homework16.java
    Included assignment instructions.

--- src/test/java/Homework16.java
+++ src/test/java/Homework16.java
@@ -8,6 +8,22 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 
 public class Homework16 extends BaseTest {
+    
+    //1. Create a new file Homework16.java in IntelliJ IDEA.
+//
+//2. Create a test case registration Navigation() using @Test annotation.
+//
+//3. Using Selenium, Navigate to "https://bbb.testpro.io/".
+//
+//4. Click Registration link.
+//
+//5. Validate that you are redirected to Registration page.
+//
+//6. Create a new branch and commit your changes.
+//
+//7. Push your code to a remote repository.
+//
+//8. Create a pull request.
 
     @Test
     public void Navigation () {
