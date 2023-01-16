ce4e2dbf45322da3f1cad5e8ea047d20aa54d346
mterlecki22 <121317526+mterlecki22@users.noreply.github.com>
2023-01-16 15:54:02 -0600

    Update Homework21.java
    Completed the assignment.

--- src/test/java/Homework21.java
+++ src/test/java/Homework21.java
@@ -24,28 +24,39 @@ import java.util.List;
 //6. Create a pull request.
 
 public class Homework21 extends BaseTest {
-        @Test
+
+    String playlistName = "Game of Thrones";
+    @Test
         public void renamePlaylist() {
 
             login("mitchelterlecki@gmail.com", "te$t$tudent");
-            doubleClickChoosePlaylist();
+            choosePlaylist();
+            contextClickChoosePlaylist();
             enterPlaylistName();
             Assert.assertTrue(doesPlaylistExist());
 
         }
-            public void doubleClickChoosePlaylist() {
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".playlist:nth-child(3)")));
-            WebElement playlistElement = driver.findElement(By.cssSelector(".playlist:nth-child(3)"));
-            action.doubleClick(playlistElement).perform();
+            public void contextClickChoosePlaylist() {
+                wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".all-songs tr.song-item:nth-child(1)")));
+                WebElement playlistElement = driver.findElement(By.cssSelector(".playlist:nth-child(3)"));
+                action.contextClick(playlistElement).perform();
+                wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("li[data-testid^='playlist-context-menu-edit']"))).click();
+            }
+        public void enterPlaylistName() {
+            WebElement playlistInputField = driver.findElement(By.cssSelector("input[name='name']"));
+            playlistInputField.sendKeys((Keys.chord(Keys.CONTROL, "a", Keys.BACK_SPACE)));
+            playlistInputField.sendKeys(playlistName);
+            playlistInputField.sendKeys(Keys.ENTER);
         }
 
-        public void enterPlaylistName() {
-        WebElement playlistInputField = driver.findElement(By.cssSelector("input[name='name']"));
-        playlistInputField.sendKeys((Keys.chord(Keys.CONTROL, "a", Keys.BACK_SPACE)));
-        playlistInputField.sendKeys("Swiss Roll Mood Playlist");
-        playlistInputField.sendKeys(Keys.ENTER);
+        public boolean doesPlaylistExist(){
+            WebElement playlistElement = driver.findElement(By.xpath("//a[text()='"+playlistName+"']"));
+            return playlistElement.isDisplayed();
+        }
+        public void choosePlaylist() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".playlist:nth-child(3)"))).click();
+        }
 
 
 
     }
-}
